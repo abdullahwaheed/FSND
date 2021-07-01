bfd56ab5705b647dc38dc183e3a8cc8cb3eb8703
Abdullah Waheed <abdullah.waheed@arbisoft.com>
2021-07-01 22:54:39 +0500

    feat: added show create api

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -651,9 +651,28 @@ def create_shows():
 def create_show_submission():
   # called to create new shows in the db, upon submitting new show listing form
   # TODO: insert form data as a new Show record in the db, instead
+  error = ''
+  try:
+    request_data = dict(request.form)
+    show = Show(**request_data)
+    
+    db.session.add(show)
+    db.session.commit()
+    flash('Show was successfully listed!')
+  except:
+    db.session.rollback()
+    flash('An error occurred. Show could not be listed.', 'error')
+    error = sys.exc_info()
+  finally:
+    db.session.close()
+  
+  if error:
+    return render_template('forms/new_show.html', form=ShowForm(request.form))
+
+  return render_template('pages/home.html')
 
   # on successful db insert, flash success
-  flash('Show was successfully listed!')
+  
   # TODO: on unsuccessful db insert, flash an error instead.
   # e.g., flash('An error occurred. Show could not be listed.')
   # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
