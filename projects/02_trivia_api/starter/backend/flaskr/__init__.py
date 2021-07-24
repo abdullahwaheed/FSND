814dfae669aaf6297c97b58979abb651d70cd509
Abdullah Waheed <abdullah.waheed@arbisoft.com>
2021-07-24 14:29:08 +0500

    feat: quiz functionality

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -1,7 +1,8 @@
+import random
+
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-import random
 
 from werkzeug.wrappers import response
 
@@ -175,15 +176,43 @@ def create_app(test_config=None):
 
   '''
   @TODO: 
-  Create a POST endpoint to get questions to play the quiz. 
-  This endpoint should take category and previous question parameters 
-  and return a random questions within the given category, 
-  if provided, and that is not one of the previous questions. 
-
   TEST: In the "Play" tab, after a user selects "All" or a category,
   one question at a time is displayed, the user is allowed to answer
   and shown whether they were correct or not. 
   '''
+  @app.route('/quizzes', methods=['POST'])
+  def quiz_question():
+    """
+      Create a POST endpoint to get questions to play the quiz. 
+      This endpoint should take category and previous question parameters and return a random questions within the given category, 
+      if provided, and that is not one of the previous questions. 
+    """
+    try:
+      request_data = request.get_json()
+      previous_questions = request_data.get('previous_questions', [])
+      quiz_category = request_data.get('quiz_category', {})
+
+      questions = Question.query.filter(~Question.id.in_(previous_questions))
+      if quiz_category.get('id'):
+        questions = questions.filter(Question.category == quiz_category.get('id'))
+
+      questions_count = questions.count()
+      if questions_count:
+        rand = random.randrange(0, questions_count)
+        question = questions[rand]
+        question = question.format()
+      else:
+        question = None
+
+      return jsonify({
+        'success': True,
+        'question': question
+      })
+
+    except Exception as exc:
+      print(exc)
+      abort(422)
+
 
   @app.errorhandler(404)
   def not_found(error):
@@ -218,5 +247,3 @@ def create_app(test_config=None):
       }), 405
   
   return app
-
-    
