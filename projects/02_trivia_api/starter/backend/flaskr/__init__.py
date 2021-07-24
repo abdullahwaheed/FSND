9ad2a97f4a6e616c587c47df767057369445cfeb
Abdullah Waheed <abdullah.waheed@arbisoft.com>
2021-07-24 15:48:51 +0500

    feat: added server error hander

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -66,6 +66,7 @@ def create_app(test_config=None):
       Create an endpoint to handle GET requests for questions, including pagination (every 10 questions). This endpoint should return a list of questions, 
       number of total questions, current category, categories.
     """
+    0/0
     questions = get_paginted_all_questions(request)
     questions.update(get_categories_data())
     return jsonify(questions)
@@ -204,5 +205,14 @@ def create_app(test_config=None):
       "error": 405,
       "message": "method not allowed"
       }), 405
+  
+  @app.errorhandler(Exception)
+  def server_error(error):
+    return jsonify({
+      "success": False, 
+      "error": 500,
+      "message": "something went wrong"
+      }), 500
+  
   
   return app
