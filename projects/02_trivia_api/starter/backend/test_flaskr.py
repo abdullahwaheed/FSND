5d33da642e774c731f24f5a60265b3fa78f4e134
Abdullah Waheed <abdullah.waheed@arbisoft.com>
2021-07-25 11:23:10 +0500

    refactor: unit tests now use env from settings

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -6,6 +6,7 @@ from werkzeug.wrappers import request
 
 from flaskr import create_app
 from models import setup_db, Question, Category
+from settings import DB_NAME, DB_HOST, DB_USER, DB_PASSWORD
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -15,8 +16,7 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgresql://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = 'postgresql://{}:{}@{}/{}_test'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
