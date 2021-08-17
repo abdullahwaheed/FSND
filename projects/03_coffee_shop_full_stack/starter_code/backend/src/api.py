6c5239a5e80db1759e05e0d3fcae87667c6e92dd
Abdullah Waheed <abdullah.waheed@arbisoft.com>
2021-08-18 00:19:48 +0500

    feat: implemented permission based create and update

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -18,7 +18,7 @@ CORS(app)
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 !! Running this funciton will add one
 '''
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 # ROUTES
 '''
@@ -54,40 +54,63 @@ def drinks_detail(payload):
 '''
 @TODO implement endpoint
     POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
+        
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def create_drink(payload):
+    """
+        it should create a new row in the drinks table
+        it should require the 'post:drinks' permission
+        it should contain the drink.long() data representation
+    """
+    try:
+        request_data = request.get_json()
+        drink = Drink(**request_data)
+        drink.insert()
+
+        return jsonify({"success": True, "drinks": [drink.long()]})
+
+    except:
+        abort(422)
 
 
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
+@app.route('/drinks/<int:drink_id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def update_drink(payload, drink_id):
+    """
         it should respond with a 404 error if <id> is not found
         it should update the corresponding row for <id>
         it should require the 'patch:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
+    """
+    try:
+        drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
 
+        if drink is None:
+            abort(404)
 
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
+        request_data = request.get_json()
+        for key, value in request_data:
+            setattr(drink, key, value)
+        
+        drink.update()
+
+        return jsonify({"success": True, "drinks": [drink.long()]})
+
+    except:
+        abort(422)
+
+@app.route('/drinks/<int:drink_id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(payload, drink_id):
+    """
         it should respond with a 404 error if <id> is not found
         it should delete the corresponding row for <id>
         it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
-@app.route('/drinks/<int:drink_id>', methods=['DELETE'])
-@requires_auth('delete:drinks')
-def delete_drink(payload, drink_id) :
+    """
     try:
         drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
 
@@ -96,8 +119,7 @@ def delete_drink(payload, drink_id) :
 
         drink.delete()
         return jsonify({"success": True, "delete": drink_id})
-    except Exception as exc:
-        print(exc)
+    except:
         abort(422)
 
 # Error Handling
