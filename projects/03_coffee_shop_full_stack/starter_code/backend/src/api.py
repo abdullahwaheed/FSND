75de4aa445dd797903c7a062c604e939b688a70a
Abdullah Waheed <abdullah.waheed@arbisoft.com>
2021-08-18 13:01:33 +0500

    feat: set frontend

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -13,52 +13,32 @@ app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
-'''
-@TODO uncomment the following line to initialize the datbase
-!! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
-!! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
-!! Running this funciton will add one
-'''
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 # ROUTES
-'''
-@TODO implement endpoint
-    GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
 @app.route('/drinks')
 def drinks():
+    """
+        it should be a public endpoint
+        it should contain only the drink.short() data representation
+    """
     drinks_data = Drink.query.all()
     drinks = [drink.short() for drink in drinks_data]
     return jsonify({"success": True, "drinks": drinks})
 
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+
 @app.route('/drinks-detail')
 @requires_auth('get:drinks-detail')
 def drinks_detail(payload):
+    """
+        it should require the 'get:drinks-detail' permission
+        it should contain the drink.long() data representation
+    """
     drinks_data = Drink.query.all()
     drinks = [drink.long() for drink in drinks_data]
     return jsonify({"success": True, "drinks": drinks})
 
 
-'''
-@TODO implement endpoint
-    POST /drinks
-        
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
 @app.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
 def create_drink(payload):
--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'abdullah-udacity-fsnd.us',
+    audience: 'coffee', // the audience set for the auth0 app
+    clientId: 'nN3IWykFm1nnBZ0sZ3JW7TjVlfDRY4oe',
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
